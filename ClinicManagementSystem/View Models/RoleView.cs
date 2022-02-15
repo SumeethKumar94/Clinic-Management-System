00d702f706f60704e455307fcf62bcfd763c0cc6
sumeeth.s <sumeeth.s@SUMEETHKUMAR>
2022-02-15 19:42:52 +0530

    New Update

---
+++ ClinicManagementSystem/View Models/RoleView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicManagementSystem.View_Models
+{
+    public class RoleView
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+    }
+}
